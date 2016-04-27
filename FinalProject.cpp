378639928a43f0d58acb083f47c1804a007a5d94
Josh Trujillo <Jotr4184@colorado.edu>
2016-04-27 14:35:30 -0600

    Added files via upload

---
+++ FinalProject.cpp
@@ -0,0 +1,210 @@
+/*
+Trip Planer (Final Project CSCI2270)
+Josh Trujillo & Ahmed Fayez
+Professor: Rhonda Hoenigman
+TA: Amber Womack
+*/
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <vector>
+#include "Travel.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    Travel Tp;
+    City *start = NULL;
+    bool budgetCheck = false;           //checking if set budget or not
+    bool planCheck = false;             //check for if you already planned the trip
+    int userInput;
+    string userName;
+
+    cout << "Please type your name: " << endl;
+    cin >> userName;
+
+    cout <<"Welcome to " << userName << "'s Vacation Extravaganza! Let us begin!" << endl;
+    cout << endl;
+
+    while(userInput != 11 && userInput != 2)
+    {
+        cout << "======Main Menu======"<< endl;
+        cout << "1.Plan Trip" << endl;
+        cout << "2.Cancel Trip" << endl;
+        cout << "3.Print Trip Path" << endl;
+        cout << "4.Set Budget" << endl;
+        cout << "5.Start Trip" << endl;
+        cout << "6.Travel to Next City" << endl;
+        cout << "7.Travel Back to Previous City" << endl;
+        cout << "8.Add City" << endl;
+        cout << "9.Delete City" << endl;
+        cout << "10.Buy Souvenir" << endl;
+        cout << "11.End Trip and Return Home" << endl;
+
+        if(start != NULL)       //once you've started your trip it displays the current city's information
+        {
+            cout <<"======City Info======" << endl;
+            cout << "Current city: " << start->cityName << endl;
+            cout << "City souvenir: " << start->souvenirName << endl;
+            cout << "Souvenir price: " << start->souvenirPrice << endl;
+            cout << "City hot-spot: " << start->hotSpot << endl;
+            cout << "Current budget: " << Tp.returnBudget() << endl;
+        }
+        cin >> userInput;
+        cin.ignore();
+
+        if(userInput == 1)      //Plan Trip/Build Linked List
+        {
+            if(planCheck == false)      //can only plan the trip once!
+            {
+                Tp.planTrip();
+                Tp.printTrip();
+                planCheck = true;
+            }
+            else
+            {
+                cout << "You have already planned your trip!" << endl;
+            }
+        }
+        if(userInput == 3)      //print trip path
+        {
+            Tp.printTrip();
+        }
+        if(userInput == 4)      //set budget
+        {
+            if(budgetCheck == true)     //checking for if already set budget
+            {
+                cout << "You have already set your budget!" << endl;
+            }
+            else
+            {
+                int setBudget;
+                cout << "Please set a budget for your trip! (Between $100 - $300)" << endl;
+                cin >> setBudget;
+
+                while(setBudget < 100 || setBudget > 300)       //has to be between 100 and 300 $$$
+                {
+                    cout << "Please set a budget as low as $100 and as high as $300." << endl;
+                    cin >> setBudget;
+                }
+
+                Tp.setBudget(setBudget);
+                budgetCheck = true;
+            }
+        }
+        if(userInput == 5)      //start trip
+        {
+            if(budgetCheck != true)     //have to set budget before starting trip check
+            {
+                cout << "Please set the budget for your trip before starting by selecting the correct menu option (4)." << endl;
+            }
+            else
+            {
+                if(start == NULL)
+                {
+                    start = Tp.startTrip(start);
+                }
+                else
+                {
+                    cout << "You have already started your trip!" << endl;
+                }
+            }
+        }
+        if(userInput == 6)      //Travel to next city
+        {
+            if(start!= NULL)     //as long as the start pointer we made at the beginning isn't NULL, it can proceed
+            {
+                if(start->next==NULL)
+                {
+                    cout<<"It seems like you're at the last stop on your list."<<endl;
+                }
+                else
+                {
+                    start=start->next;
+                }
+            }
+            else
+            {
+                cout << "You have to start your trip before going to the next city."<< endl;
+            }
+        }
+        if(userInput == 7)      //Travel back to previous city
+        {
+            if(start!=NULL)
+            {
+                if(start->previous==NULL)
+                {
+                    cout<<"It seems like you're still at the first city of your list."<<endl;
+                }
+                else
+                {
+                    start=start->previous;
+                }
+            }
+            else
+            {
+                cout<<"You have to start your trip before going to the previous city."<<endl;
+            }
+        }
+        if(userInput == 8)      //Add a city to their trip
+        {
+            string cityNew;
+            string cityPrevious;
+            string souvenirNew;
+            int souvenirNPrice;
+            string hotSpotNew;
+
+            cout << "Enter a city name: " << endl;
+            getline(cin, cityNew);
+
+            cout << "Enter a previous city name: " << endl;
+            getline(cin, cityPrevious);
+
+            cout << "Enter a souvenir for the new city (if you don't wish to add one please type NULL): " << endl;
+            getline(cin, souvenirNew);
+            if(souvenirNew != "NULL")          //doesn't ask for a price if they didn't want to set a souvenir
+            {
+                cout << "Enter a price for the souvenir please: " << endl;
+                cin >> souvenirNPrice;
+                cin.ignore();
+            }
+            cout << "Enter a hot spot location you would like to visit at this city (if you don't wish to add one please type NULL: " << endl;
+            getline(cin, hotSpotNew);
+
+            Tp.addCity(cityPrevious, cityNew, souvenirNew, souvenirNPrice, hotSpotNew);
+        }
+        if(userInput == 9)          //delete a city in trip
+        {
+            string cityDelete;
+
+            cout << "Enter a city name: " << endl;
+            cin >> cityDelete;
+
+            Tp.deleteCity(cityDelete);
+        }
+        if(userInput == 10)      //buy souvenir
+        {
+            int budget = Tp.returnBudget();
+            Tp.buySouvenir(start, budget);
+        }
+    }//End of while loop
+
+    if(userInput == 2)      //cancel trip
+    {
+        Tp.cancelTrip();
+        cout << "Your trip has been canceled!" << endl;
+    }
+
+    if(userInput == 11)     //end trip and return home
+    {
+        cout << "You have successfully ended your trip and returned home!" << endl;
+        cout << "I hope you enjoyed " << userName << "'s vacation extravaganza!" << endl;
+        cout << "You purchased: ";
+        Tp.printSouvenirs();
+        cout << endl;
+        cout << "Your remaining budget is: $" << Tp.returnBudget() << endl;
+    }
+    return 0;
+}
---
+++ Travel.cpp
@@ -0,0 +1,229 @@
+#include "Travel.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+Travel::Travel()
+{
+    head = NULL;
+    tail = NULL;
+}
+
+Travel::~Travel()
+{
+    //dtor
+    cancelTrip();
+}
+
+void Travel::planTrip()
+{
+    //note: all index's match up with all arrays, so the first index for each array is for the first city, the second is the second city and so on...
+    //build our trip with planned out cities we want to "visit"
+    string cityArray[10] = {"San Fransisco", "Boulder", "Denver", "Dallas", "Louisville", "Tallahassee", "Chicago", "Washington, D.C.", "New York", "Boston"};
+    //array of our souvenirs available for each city
+    string souvenirArray[10] = {"Bumper Sticker", "Fresh Herbs", "Concert T-Shirt", "Cowboy Star", "Cow Hide Postcard", "Phone Case", "Souvenir Mug", "Washington Monument Replica", "Teddy Bear", "Souvenir Baseball"};
+    //price of souvenirs available for each city
+    int souvenirPriceArray[10] = {15, 100, 25, 20, 15, 40, 10, 30, 40, 30};
+    //array of hot spots available for each city
+    string hotSpotArray[10] = {"Golden Gate Bridge", "Flatirons", "Summit Music Hall", "Cowboy's Stadium", "Cow fields", "Tallahassee Museum", "Willis Tower", "The White House", "Statue of Liberty", "Fenway Park"};
+    int numCities = 10;
+
+    City *cityAdded;
+    City *currentCity;
+
+    for(int i = 0; i < numCities; i++)
+    {
+        if(head == NULL)
+        {
+            cityAdded = new City(cityArray[i], NULL, NULL, souvenirArray[i], souvenirPriceArray[i], hotSpotArray[i]);
+            currentCity = cityAdded;
+            head = currentCity;
+        }
+        else
+        {
+            cityAdded = new City(cityArray[i], NULL, currentCity, souvenirArray[i], souvenirPriceArray[i], hotSpotArray[i]);
+            currentCity->next = cityAdded;
+            currentCity = cityAdded;
+
+        }
+    }
+    tail = currentCity;
+}
+
+void Travel::printTrip()
+{
+    //traverse linked list printing out each city
+    cout << "===CURRENT TRIP PATH===" << endl;
+
+    if (head == NULL)
+        cout << "NULL" << endl;
+    //If not yet built, prints out each city and then "NULL"
+    else
+    {
+        City *current = head;
+
+        cout<<"NULL <- ";
+
+        while (current->next != NULL)
+        {
+            cout << current->cityName << " <-> ";
+            current = current->next;
+        }
+        cout << current->cityName << " -> ";
+        cout << "NULL" << endl;
+    }
+
+    cout << "==================" << endl;
+
+}
+
+void Travel::setBudget(int setBudget)
+{
+    cout << "Your budget is set at $" << setBudget << endl;
+    budget = setBudget;
+    budgetCheck = true;
+}
+
+void Travel::addCity(string previousCity, string cityName, string souvenirName, int souvenirPrice, string hotSpot)
+{
+    if(previousCity == "First")         //if they want to add a city to the beginning
+    {
+        City *c = new City(cityName, head, NULL, souvenirName, souvenirPrice, hotSpot);
+        head = c;
+    }
+    else
+    {
+        City * tmp = head;
+
+        while(tmp->cityName != previousCity && tmp->next != NULL)
+        {
+            tmp = tmp->next;
+        }
+
+        City * newCity = new City(cityName, tmp->next, tmp, souvenirName, souvenirPrice, hotSpot);
+
+        tmp->next = newCity;
+    }
+    cout << "City added: " << cityName << endl;
+
+    if(souvenirName != "NULL")                                        //check if added a souvenir
+    {
+        cout << "City Souvenir: " << souvenirName << endl;
+        cout << "City Souvenir Price: $" << souvenirPrice << endl;
+    }
+    else
+    {
+        cout << "You did not add a souvenir." << endl;
+    }
+
+    if(hotSpot != "NULL")                                           //check if added a hot-spot
+    {
+        cout << "City Hot-Spot: " << hotSpot << endl;
+    }
+    else
+    {
+        cout << "You did not add a hot-spot to visit." << endl;
+    }
+    cout << "City added after: " << previousCity << endl;
+}
+
+void Travel::deleteCity(string cityNameIn)
+{
+    City *delCity = NULL;
+    City *searchCity = head;
+
+    bool found = false;                         //search for city first
+    while(!found and searchCity != NULL)
+    {
+        if(searchCity->cityName == cityNameIn)
+        {
+            found = true;
+        }
+        else
+        {
+            searchCity = searchCity->next;
+        }
+
+    }
+    if(found == true)
+    {
+        if(searchCity == head)                  //if it's the head
+        {
+            delCity = head;
+            head = head->next;
+            head->previous = NULL;
+            delete delCity;
+        }
+        else
+        {
+            searchCity->previous->next = searchCity->next;
+            searchCity->next->previous = searchCity->previous;
+            delete searchCity;
+        }
+
+        cout << cityNameIn << " has been deleted from your trip." << endl;
+    }
+    else
+    {
+        cout<<cityNameIn<<" not found"<<endl;
+    }
+}
+
+City *Travel::startTrip(City *start)
+{
+    start = head;
+    cout << "You have started your trip, the first city is: " << start->cityName << endl;
+    return head;
+}
+
+int Travel::buySouvenir(City *currentCity, int inputBudget)
+{
+    int souvenirPrice = currentCity->souvenirPrice;
+    if(souvenirPrice > inputBudget)         //check if have enough $$
+    {
+        cout << "Unfortunately you do not have the required funds to purchase." << endl;
+        budget = inputBudget;
+        return budget;
+    }
+    else
+    {
+        inputBudget = inputBudget - souvenirPrice;      //subtract from budget
+
+        souvenirsPurhcased.push_back (currentCity->souvenirName);       //push souvenir onto our purchased souvenirs list
+        budget = inputBudget;
+
+        cout << "You purchased: " << currentCity->souvenirName << "!" << endl;
+        return budget;
+    }
+}
+
+int Travel::returnBudget()      //returns the running budget set from menu option (4)
+{
+    return budget;
+}
+
+void Travel::printSouvenirs()       //loop through our souvenir purchased vector and print out contents
+{
+    for(int i=0; i<souvenirsPurhcased.size(); i++)
+        {
+            cout << souvenirsPurhcased[i];
+            cout << ", ";
+        }
+}
+
+void Travel::cancelTrip()       //delete entire trip + data
+{
+    City * tmp;
+
+    while (head != NULL)        //loop through every node
+    {
+        tmp = head;
+
+        head = head->next;
+
+        cout<<"Canceling "<<tmp->cityName<<endl;     //delete stored node 1 at a time
+        delete tmp;
+    }
+}
